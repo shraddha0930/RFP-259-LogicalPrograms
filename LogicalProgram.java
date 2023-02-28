af18df6be49c3fb7abfe517aa745777313079047
shraddha0930 <shraddhaingole11@gmail.com>
2023-02-28 08:28:23 +0530

    passed: Welcome message for logical program

---
+++ LogicalProgram.java
@@ -0,0 +1,11 @@
+package assignments.LogicalPrograms;
+
+
+public class LogicalProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Logical program");
+
+    }
+
+}
