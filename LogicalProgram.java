d18d1e9fd50f0090165dcce68c54b04f9a3f2f78
shraddha0930 <shraddhaingole11@gmail.com>
2023-02-28 08:47:34 +0530

    Added: code to fibonacci series

--- LogicalProgram.java
+++ LogicalProgram.java
@@ -1,10 +1,33 @@
 package assignments.LogicalPrograms;
 
 
+import java.util.Scanner;
+
 public class LogicalProgram {
+    Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Welcome to Logical program");
+        LogicalProgram logicalProgram = new LogicalProgram();
+        logicalProgram.fibonacciseries();
+
+
+    }
+
+    public void fibonacciseries() {
+        System.out.println("Enter fibonacci series Number:");
+
+        int n = scanner.nextInt(); //n-->fiblength
+        int firstTerm = 0, secondTerm = 1;
+
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(firstTerm + ", ");
+
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
 
     }
 
